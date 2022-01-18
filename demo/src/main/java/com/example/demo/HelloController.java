971f47e70de62ff09ffef0ad5528fdd1c8dca5ac
Grendizer <korensana@gmail.com>
2022-01-18 19:07:51 +0300

    HomeWork2_4

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+
+    @FXML private TextArea textArea;
+    @FXML private TextField textField;
+    @FXML private Button send;
+    @FXML public ListView<String> userList;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        if (!textField.getText().isEmpty()) {
+            textArea.appendText(textField.getText());
+            textArea.appendText(System.lineSeparator());
+            textField.clear();
+        }
+    }
+}
